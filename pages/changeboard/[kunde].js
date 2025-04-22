0b90cdb8d8b55243e46188721de795602b4c2299
easylog <easyloghilfe@gmail.com>
2025-04-22 21:57:49 +0200

    Update [kunde].js

--- pages/changeboard/[kunde].js
+++ pages/changeboard/[kunde].js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import clientPromise from '@/lib/mongodb';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
